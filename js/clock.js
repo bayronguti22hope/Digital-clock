1275c601a23186eb86f68ba1cd04dd6ceb6cc21a
bayronguti22hope <byrico@live.com>
2019-08-14 17:27:02 -0600

    comments

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,9 @@
 function clock(){
-    const fullDate = new Date();
-    const hours = fullDate.getHours();
-    const mins = fullDate.getMinutes();
-    const secs = fullDate.getSeconds();
-
+    const fullDate = new Date(); // Build Up function to get the date
+    const hours = fullDate.getHours(); // Getting the hours
+    const mins = fullDate.getMinutes(); // Getting the minutes
+    const secs = fullDate.getSeconds(); // Getting the seconds
+    // This will add a 0  beside to the clock if the value is lower 10
     if( hours < 10 ){
         hours = "0" + hours ;
     }
@@ -13,9 +13,10 @@ function clock(){
     if( secs < 10 ){
         secs = "0" + secs ;
     }
+    // Target all the DOM elements here
     document.getElementById('hour').innerHTML = hours;
     document.getElementById('minute').innerHTML = ":" + mins;
     document.getElementById('second').innerHTML = ":" + secs;
 }
-
+// This will update the clock every milliseconds
 setInterval(clock, 100);
